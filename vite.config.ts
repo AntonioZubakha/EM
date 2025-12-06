dc7d81bf51f5c756ea00449e2fb29fdfd08cc092
AnonioZ <anton.zubaha@gmail.com>
2025-12-06 13:38:50 +0200

    fix: set base to root for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/EM/', // Имя репозитория для GitHub Pages
+  // При кастомном домене используем корень
+  base: '/',
   plugins: [react()],
   server: {
     port: 3050,
