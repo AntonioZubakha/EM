58ae3a049560eeccb7cfebb57ebfc3c3355b1b48
AnonioZ <anton.zubaha@gmail.com>
2025-12-06 09:45:48 +0200

    fix: set base for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/EM/', // Имя репозитория для GitHub Pages
   plugins: [react()],
   server: {
     port: 3050,
